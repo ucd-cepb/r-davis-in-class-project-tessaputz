8862b69d7d225c316dccca2b3f436e3b135e1872
Tessa Putz <trputz@ucdavis.edu>
2023-11-16 14:53:38 -0800

    Updates from class 11/16

--- Scripts/Homework_7.R
+++ Scripts/Homework_7.R
@@ -5,49 +5,55 @@ colnames(gapminder)
 # create new figure: country by change in population between 2002-2007 and grouped by continents
 
 # what variables do we need? what are we calculating? ----
-# vars: country, pop change (calculating), year, continent
 
+# vars: country, pop change (calculating) => year, continent
 
 # dataframe ----
 #1. population difference between 2002 and 2007 for each country: rows of countries with column for both 2002 pop & 2007 pop
 
 data <- gapminder %>%
-  filter() %>% 
-  select() %>% 
-  pivot_???() %>% 
-  mutate()
+  filter(year == 2002 | year==2007) %>% 
+  select(country,year,pop, continent) %>% 
+  pivot_wider(names_from = year, values_from = pop) %>% 
+  mutate(pop_change = `2007`-`2002`) #note the accent as quote!
 
 data
-
-#----
-data <- gapminder %>%
-  filter(year==2002 | year ==2007) %>% 
-  select(country, continent, year, pop) %>% 
-  pivot_wider(names_from =year, values_from= pop) %>% 
-  mutate(pop_change = `2007` - `2002`)
-
 
 # plotting the data ---- 
 # basic plot
 data %>%
+  filter(continent != "Oceania" ) %>% 
   ggplot(aes(x = country, y = pop_change)) +
-  geom_col(aes(fill=continent))
+  geom_col(aes(fill=continent))+
+  facet_wrap(~continent)
 
 # 1.order country by increase in pop change 2.different axes by facet, 3. color & theme, 4.Axis labels and legend
 
-
-unique(data$continent)
-
-
-# ----
 data %>%
-  filter(continent != "Oceania") %>%
+  filter(continent != "Oceania" ) %>% 
   #reordering to sort by increase in population change
-  ggplot(aes(x = reorder(country, pop_change), y = pop_change)) + #decrease?
-  geom_col(aes(fill=continent)) +
+  ggplot(aes(x = reorder(country,pop_change), y = pop_change)) +
+  geom_col(aes(fill=continent))+
   facet_wrap(~continent, scales = "free")+
-  scale_fill_viridis_d(option ="C") + #d? others
-  theme_bw() +
+  scale_fill_viridis_d(option ="C")+
+  theme_classic()+
   labs(x = "Country", y = "Population Change Between 2002 and 2007")+
   theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-        legend.position = "none") 
+        legend.position = "none")
+  
+
+unique(data$continent)
+# if we wanted to reorder the continents
+data$continent <- factor(data$continent, levels=c("Asia","Africa","Europe","Americas","Oceania"))
+
+#run the plot again to update the order
+data %>%
+  filter(continent != "Oceania" ) %>% 
+  ggplot(aes(x = reorder(country,pop_change), y = pop_change)) +
+  geom_col(aes(fill=continent))+
+  facet_wrap(~continent, scales = "free")+
+  scale_fill_viridis_d(option ="C")+
+  theme_classic()+
+  labs(x = "Country", y = "Population Change Between 2002 and 2007")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        legend.position = "none")
--- Scripts/Week_8.R
+++ Scripts/Week_8.R
@@ -1,11 +1,11 @@
 # ---- Creating Functions ----
+#Functions take arguments as input and return an output
 
 # write a simple function to add two numbers
 my_sum <- function(a, b) {
   the_sum <- a + b
   return(the_sum)
 }
-
 my_sum(a=2,b=2)
 my_sum(2,3)
 
@@ -22,26 +22,31 @@ my_sum2(a=3)
 # temperature conversion example: Fahrenheit to Kelvin
 
 ((50- 32) * (5/ 9)) + 273.15
-
+((62- 32) * (5/ 9)) + 273.15
+((68- 32) * (5/ 9)) + 273.15
 
 #How to write a function:
 #1. Identify what piece(s) will change within your commands -- this is your argument
 #2. Remove it & replace with object(s) name(s)
 #3. Place code & arguments into the function() function
 
+
+((tempF- 32) * (5/ 9)) + 273.15
+
 F_to_K <- function(tempF){
-K <- ((tempF- 32) * (5/ 9)) + 273.15
-return(K)
+k <- ((tempF- 32) * (5/ 9)) + 273.15
+return(k)
 }
 
-F_to_K()
+((68- 32) * (5/ 9)) + 273.15
+F_to_K(68)
 
 #Pass-by-value: changes or objects within the function only exist within the function.
 
 # freezing point of water: 32 F to??
-
+F_to_K(32)
 # boiling point of water: 212 F to ??
-
+F_to_K(212)
 
 
 # ---- using dataframes in functions ----
@@ -49,18 +54,28 @@ F_to_K()
 #calculate the average GDP in a given country, in a given span of years, using the gapminder data
 
 library(tidyverse)
-gapminder <- read.csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
+gapminder <- read_csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
 
 gapminder %>% 
-  filter(country == "Canada", year %in% c(1950:1970)) %>% 
+  filter(country == "Canada", year %in% c(1950:1970)) %>%
   summarize(meanGDP = mean(gdpPercap, na.rm = T))
 
 # but what if you wanted to do this for multiple different countries? Or years? 
 # turn it into a function! 
 
+avgGDP <- function(COUNTRY,YR.RANGE){
+  gdp <- gapminder %>% 
+    filter(country == COUNTRY, year %in% c(YR.RANGE)) %>%
+    summarize(meanGDP = mean(gdpPercap, na.rm = T))
+  return(gdp)
+  }
+avgGDP(COUNTRY = "Iran",YR.RANGE = 1980:1985)
+avgGDP(COUNTRY = "Iran",YR.RANGE = 1980:1985)
+
+
 avgGDP <- function(cntry,yr.range){
   df <- gapminder %>% 
-    filter(country == cntry, year %in% c(yr.range)) %>% 
+    filter(country == cntry, year %in% c(yr.range)) %>%
     summarize(meanGDP = mean(gdpPercap, na.rm = T))
   return(df)
   }
@@ -72,31 +87,30 @@ surveys <- read_csv("data/portal_data_joined.csv")
 surveys %>% 
   filter(species_id == "PE" & !is.na(weight) & !is.na(hindfoot_length)) %>% 
   ggplot(aes(x=weight, y=hindfoot_length))+
-  geom_point(alpha=.5)+
+  geom_jitter(alpha=.5)+
   labs(x="Weight", y="Hindfoot length", title = "PE weight x hindfoot")
   
 surveys %>% 
   filter(species_id == "PL" & !is.na(weight) & !is.na(hindfoot_length)) %>% 
   ggplot(aes(x=weight, y=hindfoot_length))+
-  geom_point(alpha=.5)+
+  geom_jitter(alpha=.5)+
   labs(x="Weight", y="Hindfoot length", title = "PL weight x hindfoot")
 
 surveys %>% 
   filter(species_id == "SH" & !is.na(weight) & !is.na(hindfoot_length)) %>% 
   ggplot(aes(x=weight, y=hindfoot_length))+
-  geom_point(alpha=.5)+
+  geom_jitter(alpha=.5)+
   labs(x="Weight", y="Hindfoot length", title = "SH weight x hindfoot")
 
 # instead of copying and pasting make a function!
 
-#----
-plot_fx <- function(SPECIES,PLOTTITLE) {
+plot_fx <- function(SPECIES,PLOTTITLE="Tessa's cool plot"){
   surveys %>% 
-  filter(species_id == SPECIES & !is.na(weight) & !is.na(hindfoot_length)) %>% 
-  ggplot(aes(x=weight, y=hindfoot_length))+
-  geom_point(alpha=.5)+
-  labs(x="Weight", y="Hindfoot length", title = PLOTTITLE)
-}
-
-plot_fx("SH", "SH weight x hindfoot")
-plot_fx("SH") #give a default title 
+    filter(species_id == SPECIES & !is.na(weight) & !is.na(hindfoot_length)) %>% 
+    ggplot(aes(x=weight, y=hindfoot_length))+
+    geom_jitter(alpha=.5)+
+    labs(x="Weight", y="Hindfoot length", title = PLOTTITLE)
+  }
+ 
+plot_fx(SPECIES = "SH",PLOTTITLE = "SH weight x hindfoot") 
+plot_fx(SPECIES = "SH") #using the default plot title
